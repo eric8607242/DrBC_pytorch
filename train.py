8f870c194b64d3d5af89ce471e16f090f8e4b94d
eric8607242 <e0928021388@gmail.com>
2020-03-20 17:17:54 +0800

    dataset, train

--- train.py
+++ train.py
@@ -9,44 +9,21 @@ import torch.nn.functional as F
 from torch_geometric.utils import from_networkx
 
 from utils.model import DrBC
+from utils.dataflow import GraphData
 
-def generate_graph():
-    n = 20
-    g = nx.powerlaw_cluster_graph(n=n, m=4, p=0.05)
+BATCH_SIZE = 16
 
-    degree = g.degree([i for i in range(n)])
-    degree = [i[1] for i in degree]
+def wrap_data(data, dtype=None, cuda=True):
+    data = torch.tensor(data) if dtype is None else torch.tensor(data, dtype=dtype)
+    data = data.cuda() if cuda else data
+    return data
 
-    bc = nx.betweenness_centrality(g)
-    bc = [v for _, v in bc.items()]
+def calculate_loss(outs, label, source_ids, target_ids):
+    pred = torch.sigmoid(outs[source_ids] - outs[target_ids])
+    gt = torch.sigmoid(label[source_ids] - label[target_ids])
 
-    g = from_networkx(g)
-    edge_index = g.edge_index
-
-    return degree, bc, edge_index
-
-def get_data(degree, bc):
-    x = [[deg, 1, 1] for deg in degree]
-    y = [[b] for b in bc]
-
-    x = torch.Tensor(x)
-    y = torch.Tensor(y)
-
-    return x, y
-
-def get_batch_pair(gt, outs):
-    node_nums = gt.shape[0]
-
-    node_combine = list(combinations([i for i in range(node_nums)], 2))
-
-    node_pairs = torch.zeros((len(node_combine), 2))
-    for index, c in enumerate(node_combine):
-        i, j = c
-        node_pairs[index, 0] = gt[i]-gt[j]
-        node_pairs[index, 1] = outs[i]-outs[j]
-
-    return node_pairs
-
+    loss = F.binary_cross_entropy(pred, gt, reduction="sum")
+    return loss
 
 
 def train():
@@ -56,21 +33,25 @@ def train():
 
     for iteration in range(5000):
         if iteration % 500 == 0:
-            degree, bc, edge_index = generate_graph()
-            x, y = get_data(degree, bc)
-            x = x.cuda()
-            y = y.cuda()
-            edge_index = edge_index.cuda()
+            g = GraphData()
 
+            edge_index = g.get_edge_index()
 
-        outs = model(x, edge_index)
-        node_pairs = get_batch_pair(y, outs)
+            train_data = self.get_train_data()
+            label = g.get_label()
 
-        loss = F.binary_cross_entropy(torch.sigmoid(node_pairs[:, 1]), torch.sigmoid(node_pairs[:, 0].detach()), reduction="sum")
+            edge_index = wrap_data(edge_index)
+            train_data = wrap_data(train_data)
+            label = wrap_data(label)
+
+        source_ids, target_ids = g.get_source_target_pairs()
+
+        outs = model(train_data, edge_index)
+        loss = calculate_loss(outs, label, source_ids, target_ids)
+
         print(loss)
         loss.backward()
         optimizer.step()
-
 
 
 
---
+++ utils/dataflow.py
@@ -0,0 +1,91 @@
+import random
+
+import numpy as np
+import networkx as nx
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+
+from torch_geometric.utils import from_networkx
+
+
+class GraphData:
+    def __init__(self, batch_size=16):
+        self.graph_list = [self._generate_graph() for i in range(batch_size)]
+
+    def _generate_graph(self, num_max=200, num_min=100):
+        node_nums = np.random.randint(num_max-num_min+1) + num_min
+        g = nx.powerlaw_cluster_graph(n=node_nums, m=4, p=0.05)
+
+        return g
+
+    def _get_node_degree(self):
+        degree_list = [graph.degree() for graph in self.graph_list]
+
+        degree_value = []
+        for degree in degree_list:
+            degree_value.extend([v for _, v in degree])
+
+
+        return degree_value
+
+    def get_train_data(self):
+        degree_value = self._get_node_degree()
+        train_data = [[deg, 1, 1] for deg in degree_value]
+        
+        return train_data
+
+
+
+    def get_edge_index(self):
+        id_nums = 0
+        edge_index = []
+        for graph in self.graph_list:
+            node_nums = len(graph.nodes)
+            edge_index.extend([[v1+id_nums, v2+id_nums] for v1, v2 in graph.edges])
+
+            id_nums += node_nums
+
+        return edge_index
+
+    def get_label(self):
+        bc_list = [nx.betweenness_centrality(graph) for graph in self.graph_list]
+        labels = [bc.values for bc in bc_list]
+
+        return labels
+
+    def get_source_target_pairs(self, repeat=5):
+        id_nums = 0
+        source_ids = []
+        target_ids = []
+        
+        for graph in self.graph_list:
+            node_nums = len(graph.nodes)
+
+            source_id = [i for i in range(id_nums, node_nums+id_nums)]
+            target_id = [i for i in range(id_nums, node_nums+id_nums)]
+
+            source_id *= 5
+            target_id *= 5
+
+            random.shuffle(source_id)
+            random.shuffle(target_id)
+
+            source_ids.extend(source_id)
+            target_ids.extend(target_id)
+
+            id_nums += node_nums
+        return source_ids, target_ids
+
+
+            
+                    
+
+
+
+
+g = GraphData(batch_size=2)
+g.get_node_degree()
+g.get_source_target_pairs()
+g.get_edge_index()
